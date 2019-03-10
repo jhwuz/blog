11b7c3f77867d8d8763f65ec260c2c17beb17fb9
jhwuz <jeremyhaowu@gmail.com>
2019-03-10 00:47:38 -0700

    add registration page

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,14 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Jeremy',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2019',
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2019',
-    },
-]
+from .models import Post
 
 
 def home(request):
-    context = {
-        'posts': posts
-    }
+  context = {
+    'posts': Post.objects.all()
+  }
 
-    return render(request, 'blog/home.html', context)
+  return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
+  return render(request, 'blog/about.html', {'title': 'About'})
