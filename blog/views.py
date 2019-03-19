8f70ba644814f8c4bad2a5f3e4d5a62d124f74e2
jhwuz <jeremyhaowu@gmail.com>
2019-03-19 00:14:10 -0600

    add post creation, delete, and update.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import (
-  ListView, DetailView, CreateView)
+  ListView, DetailView, CreateView, UpdateView, DeleteView)
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def home(request):
@@ -22,13 +23,39 @@ class PostDetailView(DetailView):
   model = Post
 
 
-class PostCreateView(CreateView):
+class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+  model = Post
+  success_url = '/'
+
+  def test_func(self):
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
   model = Post
   fields = ['title', 'content', ]
 
   def form_valid(self, form):
     form.instance.author = self.request.user
     return super().form_valid(form)
+
+
+class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+  model = Post
+  fields = ['title', 'content', ]
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+  def test_func(self):
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
 
 
 def about(request):
